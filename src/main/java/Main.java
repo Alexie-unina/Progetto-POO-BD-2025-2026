99faab39fd398c8831fae364e238716681927f30
Alessandro Pizzi <alessandrett@gmail.com>
2026-05-04 21:48:22 +0200

    Interfaccia Base

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
-
+import model.Prenotazione;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("30 e lode");
+        new InterfacciaCLI().interfacciaCli();
     }
 }
--- src/main/java/model/Hostess.java
+++ src/main/java/model/Hostess.java
@@ -2,15 +2,25 @@ package model;
 
 public class Hostess extends Utente{
     private final String idHostess;
-    public Hostess(String login,String password,String nomeCompleto, String codiceFiscale, String idHostess){
+    private double salario;
+    public Hostess(String login,String password,String nomeCompleto, String codiceFiscale, String idHostess,double salario){
         super(login,password, nomeCompleto, codiceFiscale);
         this.idHostess = idHostess;
+        this.salario = salario;
     }
-    public Hostess(String login,String password, String nomeCompleto,  String codiceFiscale, String numeroDiCellulare, String idHostess){
+    public Hostess(String login,String password, String nomeCompleto,  String codiceFiscale, String numeroDiCellulare, String idHostess,double salario){
         super(login,password, nomeCompleto, codiceFiscale, numeroDiCellulare);
         this.idHostess = idHostess;
+        this.salario = salario;
     }
      public String getIdHostess(){
         return idHostess;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public double getSalario(){
+        return salario;
     }
 }
