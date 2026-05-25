8ac39303e2e50d93d526c37f56e85bfbab343946
Alex123310 <alessandrett@gmail.com>
2026-05-25 19:42:20 +0200

    commit

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,6 +8,21 @@ public class  Controller {
     public void exit(){
         System.exit(0);
     }
+    /* login non inserito
+       password non inserita
+       nome non inserito
+       idPilota non inserito
+       codiceFiscale non inserito
+       salario non inserito
+       codiceFiscale non valido
+       numeroDiCellulare non valido
+       salario non valido
+       login già esistente
+       idPilota già esistente
 
+
+
+
+     */
 
 }
